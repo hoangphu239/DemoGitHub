ace9afe3fdd373c87a71f72076339dc6072fb09d
hoangphu239 <hoangphu239@gmail.com>
2024-08-30 08:42:32 +0700

    New layout activity

--- app/src/main/java/com/phule/demogithub/MainActivity.kt
+++ app/src/main/java/com/phule/demogithub/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,7 +23,7 @@ class MainActivity : ComponentActivity() {
             DemoGitHubTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android 3",
+                        name = "Android New Layout",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,10 +34,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    LazyColumn {
+        items(20) {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
